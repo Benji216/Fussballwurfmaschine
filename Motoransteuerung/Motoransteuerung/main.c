eb891ff680ee266f3f502a4b62ce656f09d58b82
Benji216 <benjamin.gassner.05@gmail.com>
2019-11-20 13:56:23 +0100

    H

--- Motoransteuerung/Motoransteuerung/main.c
+++ Motoransteuerung/Motoransteuerung/main.c
@@ -13,18 +13,17 @@ unsigned short int timer0zeit = 0;
 
 int main (void)
 {
-	 //char buffer[20];    //Wird benötigt um am LCD Ziffern auszugeben
+	 char buffer[20];    //Wird benötigt um am LCD Ziffern auszugeben
 	 
 	allinit();
 	
 	//Fault_Protection();
 	
-	/*itoa (duty,buffer,10);
-	LCD_cmd(0x01);
-	LCD_string(buffer);*/
 	
 	while(1)
 	{
+		LCD_cmd(0xC5); //gehe zu 2. Zeile, 6. Position
+		LCD_send('a');
 		if(duty1 >= 0 && duty1 <= 100)
 		{
 			OCR4A = duty1 *  2;//PWM1 Tastverhältnis einstellen
@@ -33,9 +32,6 @@ int main (void)
 		{
 			OCR4B = duty2 *  2;//PWM2 Tastverhältnis einstellen
 		}
-			/*itoa (duty,buffer,10);
-			LCD_cmd(0x01);
-			LCD_string(buffer);*/
 	}
 	return 0;
 }
@@ -80,6 +76,7 @@ ISR (INT2_vect) // Wenn Taster an PD2 gedrückt
 	}
 	dutyh = 3;
 	TCCR0B = TCCR0B | (1<<CS01)|(1<<CS00);//:64 Teiler => dt=64/8MHz=8us startet timer0
+	timer0h = 0;
 	timer0zeit = 700; //700ms
 }
 ISR (INT3_vect) // Wenn Taster an PD3 gedrückt
@@ -96,68 +93,55 @@ ISR (INT3_vect) // Wenn Taster an PD3 gedrückt
 ISR (TIMER0_OVF_vect)
 {
 	timer0h++;
+	if(((PIND & (1<<PIND0)) == 1) && ((PIND & (1<<PIND1)) == 1) && ((PIND & (1<<PIND2)) == 1) && ((PIND & (1<<PIND3)) == 1))
+	{
+		TCCR0B = TCCR0B &~ (1<<CS01) &~ (1<<CS00);//stoppt timer0
+		timer0h = 0;
+	}
 	if(timer0h > timer0zeit)
 	{
 		switch (dutyh)
 		{
-			case 0:
-				TCCR0B = TCCR0B &~ (1<<CS01) &~ (1<<CS00);//stoppt timer0
-				break;
 			case 1: // Taster 1
-				if ((PIND & (1<<PIND0)) == 0x00) //wenn PD0 = LOW
+				if ((PIND & (1<<PIND0)) == 0) //wenn PD0 = LOW
 				{
 					if(duty1 < 100)
 					{
 						duty1++;
 					}
 				}
-				else
-				{
-					dutyh = 0;
-				}
 				break;
 			case 2: //Taster 2
-				if ((PIND & (1<<PIND1)) == 0x00) //wenn PD1 = LOW
+				if ((PIND & (1<<PIND1)) == 0) //wenn PD1 = LOW
 				{
 					if(duty1 > 0)
 					{
 						duty1--;
 					}
 				}
-				else
-				{
-					dutyh = 0;
-				}
 				break;
 				case 3: //Taster 3
-				if ((PIND & (1<<PIND2)) == 0x00) //wenn PD2 = LOW
+				if ((PIND & (1<<PIND2)) == 0) //wenn PD2 = LOW
 				{
 					if(duty2 < 100)
 					{
 						duty2++;
 					}
 				}
-				else
-				{
-					dutyh = 0;
-				}
 				break;
 				case 4:
-				if ((PIND & (1<<PIND3)) == 0x00) //wenn PD3 = LOW
+				if ((PIND & (1<<PIND3)) == 0) //wenn PD3 = LOW
 				{
 					if(duty2 > 0)
 					{
 						duty2--;
 					}
 				}
-				else
-				{
-					dutyh = 0;
-				}
 				break;
 			default:
 				{
 					TCCR0B = TCCR0B &~ (1<<CS01) &~ (1<<CS00);//stoppt timer0
+					timer0h = 0;
 					dutyh = 0;
 					break;
 				}
@@ -165,6 +149,8 @@ ISR (TIMER0_OVF_vect)
 		timer0zeit = 50; // 50 ms
 		timer0h = 0;
 	}
+	
+	
 	TCNT0 = 256-125; //Reinitialisierung
 }
 
